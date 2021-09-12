9b6e1057129be905907939525ab551714c866201
ahmedzatar <ahmed.zatar2@gmail.com>
2021-09-13 02:47:21 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import { Route } from "react-router";
+import { Route, Switch } from "react-router";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 function App() {
   return (
     <div>
-      <header></header>
+      <MainHeader />
       <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+        </Switch>
       </main>
     </div>
   );
