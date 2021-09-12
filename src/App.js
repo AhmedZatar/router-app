cca031519885999ba88ab220e9fa0102c434eb52
AhmedZatar <ahmed.zatar97@gmail.com>
2021-09-12 19:01:41 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import { Route } from "react-router";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <header></header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
